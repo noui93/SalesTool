578a2d7e421d577400326f6f08ab0817bb7ffedb
noui dz <rahmani.med.menouar@gmail.com>
2016-11-16 19:36:22 +0100

    chnages changes changes

---
+++ SalesToolRepository/project2/src/project2/Helloworld.java
@@ -0,0 +1,13 @@
+package project2;
+
+public class Helloworld {
+
+	public static void afficher(){
+		System.out.println("bonjour tous le monde 1");
+	}
+	public static void main(String[] args) {
+		
+afficher();
+	}
+
+}
