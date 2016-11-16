41173200de7235009cf76770dbf7303a1e6e77b9
noui dz <rahmani.med.menouar@gmail.com>
2016-11-16 19:53:23 +0100

    second changes

--- SalesToolRepository/project2/src/project2/Helloworld.java
+++ SalesToolRepository/project2/src/project2/Helloworld.java
@@ -4,6 +4,7 @@ public class Helloworld {
 
 	public static void afficher(){
 		System.out.println("bonjour tous le monde 1");
+		System.out.println("hello world again in english");
 	}
 	public static void main(String[] args) {
 		
